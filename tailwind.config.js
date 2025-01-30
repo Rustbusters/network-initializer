1ccc8be2ad85e0d1c7665fdf4854aa2a0219e213
Isax03 <isaia.tonini@gmail.com>
2025-01-30 08:17:57 +0100

    changed gitignore, removed useless tailwind config

--- tailwind.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import('tailwindcss').Config} */
-module.exports = {
-    content: ["./static/**/*.{html,js}"],
-    darkMode: 'class',
-    theme: {
-        extend: {},
-    },
-    plugins: [],
-}
